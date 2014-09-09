b658fa4aa192ceb1cc08f26fb224ac4f5e55f86f
Daniel Romero <danielromeroauk@gmail.com>
2014-09-09 13:32:23 -0500

    En inicio se ve cuantas remisiones pendientes hay.
    Se creó un enlace al informe del tipo de remisión correspondiente, con
    el filtro estado = pendiente.

--- app/views/inicio.blade.php
+++ app/views/inicio.blade.php
@@ -2,6 +2,62 @@
 
 @section('content')
 
-<img src="img/inventarios.jpg" alt="inventarios" class="img-responsive" />
+	<div class="col-md-3">
+		<img src="img/inventarios.jpg" alt="inventarios" class="img-responsive" />
+	</div>
+
+	<div class="col-md-6">
+
+		<h1>Remisiones pendientes</h1>
+
+	    <table class="table table-striped table-hover table-bordered">
+	    	<tr>
+				<th>Tipo</th>
+				<th class="center">Cantidad</th>
+				<th>Acción</th>
+	    	</tr>
+	    	<tr>
+	    		<td>Compras</td>
+	    		<td class="center">{{ Purchase::where('status', '=', 'pendiente')->count() }}</td>
+	    		<td>
+	    			<a href="{{ url('purchases/filter-by-status?estado=pendiente') }}">
+	    				<span class="glyphicon glyphicon-search"></span>
+	    				Ver informe
+	    			</a>
+	    		</td>
+	    	</tr>
+	    	<tr>
+	    		<td>Ventas</td>
+	    		<td class="center">{{ Sale::where('status', '=', 'pendiente')->count() }}</td>
+	    		<td>
+	    			<a href="{{ url('sales/filter-by-status?estado=pendiente') }}">
+	    				<span class="glyphicon glyphicon-search"></span>
+	    				Ver informe
+	    			</a>
+	    		</td>
+	    	</tr>
+	    	<tr>
+	    		<td>Daños</td>
+	    		<td class="center">{{ Damage::where('status', '=', 'pendiente')->count() }}</td>
+	    		<td>
+	    			<a href="{{ url('damages/filter-by-status?estado=pendiente') }}">
+	    				<span class="glyphicon glyphicon-search"></span>
+	    				Ver informe
+	    			</a>
+	    		</td>
+	    	</tr>
+	    	<tr>
+	    		<td>Rotaciones</td>
+	    		<td class="center">{{ Rotation::where('status', '=', 'pendiente')->count() }}</td>
+	    		<td>
+	    			<a href="{{ url('rotations/filter-by-status?estado=pendiente') }}">
+	    				<span class="glyphicon glyphicon-search"></span>
+	    				Ver informe
+	    			</a>
+	    		</td>
+	    	</tr>
+	    </table>
+
+	</div>
 
 @stop
